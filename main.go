2df2dde52ce7a9f2f67078e6afd9d7020e02c125
egoroff <egoroff@gmail.com>
2018-09-08 19:12:50 +0300

    Path walking experiments with some output

--- main.go
+++ main.go
@@ -2,9 +2,12 @@
 package main
 
 import (
+	"fmt"
+	"github.com/dustin/go-humanize"
 	"github.com/voxelbrain/goptions"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 type Options struct {
@@ -20,4 +23,24 @@ func main() {
 	if _, err := os.Stat(options.Path); os.IsNotExist(err) {
 		log.Fatalf("Directory '%s' does not exist. Details:\n  %v", options.Path, err)
 	}
+
+	countFiles := 0
+	countDirs := 0
+	var totalSize uint64
+	filepath.Walk(options.Path, func(path string, info os.FileInfo, err error) error {
+		fmt.Printf("%s", path)
+		if info.IsDir() {
+			countDirs++
+		} else {
+			countFiles++
+			sz := uint64(info.Size())
+			fmt.Printf(" (%s)", humanize.Bytes(sz))
+			totalSize += sz
+		}
+		fmt.Print("\n")
+		return nil
+	})
+
+	fmt.Printf("Total files %d Total size: %s\n", countFiles, humanize.Bytes(totalSize))
+	fmt.Printf("Total folders %d\n", countDirs)
 }
