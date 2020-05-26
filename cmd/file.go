46a8d3e845e57a096cbcd48fa8562944160ad708
egoroff <egoroff@gmail.com>
2020-05-26 22:16:23 +0300

    by default extension statistic not shown in file command. You can enable it by flag -e

--- cmd/file.go
+++ cmd/file.go
@@ -8,6 +8,8 @@ import (
 	"path/filepath"
 )
 
+const extParamName = "ext"
+
 // fileCmd represents the file command
 var fileCmd = &cobra.Command{
 	Use:     "fi",
@@ -32,6 +34,12 @@ var fileCmd = &cobra.Command{
 			return err
 		}
 
+		showExtStatistic, err := cmd.Flags().GetBool(extParamName)
+
+		if err != nil {
+			return err
+		}
+
 		opt := options{Verbosity: verbose, Path: path, Range: ranges}
 
 		if _, err := appFileSystem.Stat(opt.Path); os.IsNotExist(err) {
@@ -49,7 +57,15 @@ var fileCmd = &cobra.Command{
 		totalmod := module.NewTotalModule(ctx)
 		rangemod := module.NewRangeModule(ctx, opt.Verbosity, opt.Range)
 		totalfilemod := module.NewTotalFileModule(ctx)
-		extmod := module.NewExtensionModule(ctx)
+
+		var extmod module.Module
+
+		if showExtStatistic {
+			extmod = module.NewExtensionModule(ctx)
+		} else {
+			extmod = module.NewExtensionHiddenModule(ctx)
+		}
+
 		topfilesmod := module.NewTopFilesModule(ctx)
 
 		modules := []module.Module{totalfilemod, extmod, topfilesmod, foldersmod, rangemod, totalmod}
@@ -66,4 +82,5 @@ func init() {
 	fileCmd.Flags().StringP(pathParamName, "p", "", "REQUIRED. Directory path to show info.")
 	fileCmd.Flags().IntSliceP(rangeParamName, "r", []int{}, "Output verbose files info for range specified. Range is the number between 1 and 10")
 	fileCmd.Flags().BoolP(verboseParamName, "v", false, "Be verbose")
+	fileCmd.Flags().BoolP(extParamName, "e", false, "Show extensions statistic. By default false")
 }
