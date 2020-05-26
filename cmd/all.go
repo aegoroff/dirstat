24e65c70ea37d3b72dbfb6efc20eeeb38c49a91b
egoroff <egoroff@gmail.com>
2020-05-26 18:33:31 +0300

    All info output moved to all command instead of root

---
+++ cmd/all.go
@@ -0,0 +1,59 @@
+package cmd
+
+import (
+	"dirstat/module"
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+	"path/filepath"
+)
+
+// allCmd represents the all command
+var allCmd = &cobra.Command{
+	Use:     "a",
+	Aliases: []string{"all"},
+	Short:   "Show all information about folder/volume",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		path, err := cmd.Flags().GetString(pathParamName)
+
+		if err != nil {
+			return err
+		}
+
+		ranges, err := cmd.Flags().GetIntSlice(rangeParamName)
+
+		if err != nil {
+			return err
+		}
+
+		verbose, err := cmd.Flags().GetBool(verboseParamName)
+
+		if err != nil {
+			return err
+		}
+
+		opt := options{Verbosity: verbose, Path: path, Range: ranges}
+
+		if _, err := appFileSystem.Stat(opt.Path); os.IsNotExist(err) {
+			return err
+		}
+
+		if opt.Path[len(opt.Path)-1] == ':' {
+			opt.Path = filepath.Join(opt.Path, "\\")
+		}
+
+		_, _ = fmt.Fprintf(appWriter, "Root: %s\n\n", opt.Path)
+
+		module.Execute(opt.Path, appFileSystem, appWriter, opt.Verbosity, opt.Range)
+
+		printMemUsage(appWriter)
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(allCmd)
+	allCmd.Flags().StringP(pathParamName, "p", "", "REQUIRED. Directory path to show info.")
+	allCmd.Flags().IntSliceP(rangeParamName, "r", []int{}, "Output verbose files info for range specified. Range is the number between 1 and 10")
+	allCmd.Flags().BoolP(verboseParamName, "v", false, "Be verbose")
+}
--- cmd/root.go
+++ cmd/root.go
@@ -1,14 +1,12 @@
 package cmd
 
 import (
-	"dirstat/module"
 	"fmt"
 	"github.com/dustin/go-humanize"
 	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
 	"io"
 	"os"
-	"path/filepath"
 	"runtime"
 )
 
@@ -31,50 +29,13 @@ var rootCmd = &cobra.Command{
 	Short: "Directory statistic tool",
 	Long:  ` A small tool that shows selected folder or drive (on Windows) usage statistic`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-
-		path, err := cmd.Flags().GetString(pathParamName)
-
-		if err != nil {
-			return err
-		}
-
-		ranges, err := cmd.Flags().GetIntSlice(rangeParamName)
-
-		if err != nil {
-			return err
-		}
-
-		verbose, err := cmd.Flags().GetBool(verboseParamName)
-
-		if err != nil {
-			return err
-		}
-
-		opt := options{Verbosity: verbose, Path: path, Range: ranges}
-
-		if _, err := appFileSystem.Stat(opt.Path); os.IsNotExist(err) {
-			return err
-		}
-
-		if opt.Path[len(opt.Path)-1] == ':' {
-			opt.Path = filepath.Join(opt.Path, "\\")
-		}
-
-		_, _ = fmt.Fprintf(appWriter, "Root: %s\n\n", opt.Path)
-
-		module.Execute(opt.Path, appFileSystem, appWriter, opt.Verbosity, opt.Range)
-
-		printMemUsage(appWriter)
-		return nil
+		return cmd.Help()
 	},
 }
 
 func init() {
 	cobra.MousetrapHelpText = ""
 	appWriter = os.Stdout
-	rootCmd.Flags().StringP(pathParamName, "p", "", "REQUIRED. Directory path to show info.")
-	rootCmd.Flags().IntSliceP(rangeParamName, "r", []int{}, "Output verbose files info for range specified. Range is the number between 1 and 10")
-	rootCmd.Flags().BoolP(verboseParamName, "v", false, "Be verbose")
 }
 
 // Execute starts package running
