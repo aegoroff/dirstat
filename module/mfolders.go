a00325dd0ec86edf4b908a912f04ba9380d35ce4
egoroff <egoroff@gmail.com>
2020-05-29 23:47:27 +0300

    redundant local variable removed

--- module/mfolders.go
+++ module/mfolders.go
@@ -104,11 +104,10 @@ func (m *foldersWorker) postScan() {
 }
 
 func (m *foldersWorker) folderHandler(fe *sys.FolderEntry) {
-	var cmp rbtree.Comparable
-	cmp = &folderNode{
+	fn := &folderNode{
 		container{name: fe.Path, count: fe.Count, size: fe.Size},
 	}
-	m.folders.Insert(rbtree.NewNode(cmp))
+	m.folders.Insert(rbtree.NewNode(fn))
 }
 
 func (m *foldersWorker) fileHandler(_ *sys.FileEntry) {
