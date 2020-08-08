d2a61fc848ea1d54474e6b2728c619f364c359d4
egoroff <egoroff@gmail.com>
2020-08-08 07:37:43 +0300

    unused function RunUnderWindows() removed

--- module/internal/sys/util.go
+++ module/internal/sys/util.go
@@ -3,13 +3,7 @@ package sys
 import (
 	"io"
 	"log"
-	"runtime"
 )
-
-// RunUnderWindows gets whether code running under Microsoft Windows
-func RunUnderWindows() bool {
-	return runtime.GOOS == "windows"
-}
 
 // Close wraps io.Closer Close func with error handling
 func Close(c io.Closer) {
