0d0a3c1cc1af34a9093f2222485f010ca92f1518
egoroff <egoroff@gmail.com>
2020-08-14 11:12:28 +0300

    scanning test added

---
+++ module/internal/sys/scan_test.go
@@ -0,0 +1,35 @@
+package sys
+
+import (
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_Scan(t *testing.T) {
+	// Arrange
+	ass := assert.New(t)
+	appFS := afero.NewMemMapFs()
+	_ = appFS.MkdirAll("/f/s", 0755)
+	_ = afero.WriteFile(appFS, "/f/f.txt", []byte("123"), 0644)
+	_ = afero.WriteFile(appFS, "/f/s/f.txt", []byte("1234"), 0644)
+	var folders int
+	var files int
+	h := func(f *ScanEvent) {
+		if f.File != nil {
+			files++
+		}
+		if f.Folder != nil {
+			folders++
+		}
+	}
+	var handlers []ScanHandler
+	handlers = append(handlers, h)
+
+	// Act
+	Scan("/", appFS, handlers)
+
+	// Assert
+	ass.Equal(2, files)
+	ass.Equal(3, folders)
+}
