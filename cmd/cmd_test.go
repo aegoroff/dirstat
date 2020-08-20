31c3efc7ef4e60fa56646ec96305d8175e1bd8d9
egoroff <egoroff@gmail.com>
2020-08-20 21:46:59 +0300

    integration test improved to increase coverage

--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 )
 
@@ -15,12 +16,18 @@ func Test_PositiveTests(t *testing.T) {
 	_ = appFS.MkdirAll("/f/s", 0755)
 	_ = afero.WriteFile(appFS, "/f/f.txt", []byte("123"), 0644)
 	_ = afero.WriteFile(appFS, "/f/s/f.txt", []byte("1234"), 0644)
+	_ = afero.WriteFile(appFS, "/f/f2.txt", []byte("12345"), 0644)
+	_ = afero.WriteFile(appFS, "/f/f3.txt", []byte("123456"), 0644)
+	_ = afero.WriteFile(appFS, "/f/f4.txt", []byte("1234567"), 0644)
+	_ = afero.WriteFile(appFS, "/f/f5.txt", []byte("12345678"), 0644)
+	_ = afero.WriteFile(appFS, "/f/f6.txt", []byte("12345678910"), 0644)
 
 	var tests = []struct {
 		cmdline []string
 	}{
 		{[]string{"a", "-p", "/"}},
 		{[]string{"a", "-p", "/", "-r", "1"}},
+		{[]string{"a", "-p", "/f", "-r", "1", "-o"}},
 		{[]string{"a", "-p", "/", "-m"}},
 		{[]string{"a", "-p", "/f", "-o"}},
 		{[]string{"b", "-p", "/"}},
@@ -38,6 +45,8 @@ func Test_PositiveTests(t *testing.T) {
 
 		// Assert
 		ass.NoError(err)
-		fmt.Print(w.String())
+		fmt.Println(strings.Join(test.cmdline, " "))
+		fmt.Println("----------------------------------------------")
+		fmt.Println(w.String())
 	}
 }
