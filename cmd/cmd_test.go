3f9442f0dad247095bd2f74534f8d6a477c180f1
egoroff <egoroff@gmail.com>
2020-08-21 00:19:40 +0300

    more integration tests

--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -15,17 +15,18 @@ func Test_PositiveTests(t *testing.T) {
 	appFS := afero.NewMemMapFs()
 	_ = appFS.MkdirAll("/f/s", 0755)
 	_ = afero.WriteFile(appFS, "/f/f.txt", []byte("123"), 0644)
-	_ = afero.WriteFile(appFS, "/f/s/f.txt", []byte("1234"), 0644)
-	_ = afero.WriteFile(appFS, "/f/f2.txt", []byte("12345"), 0644)
-	_ = afero.WriteFile(appFS, "/f/f3.txt", []byte("123456"), 0644)
-	_ = afero.WriteFile(appFS, "/f/f4.txt", []byte("1234567"), 0644)
-	_ = afero.WriteFile(appFS, "/f/f5.txt", []byte("12345678"), 0644)
-	_ = afero.WriteFile(appFS, "/f/f6.txt", []byte("12345678910"), 0644)
+	_ = afero.WriteFile(appFS, "/f/s/f.md", []byte("1234"), 0644)
+	_ = afero.WriteFile(appFS, "/f/f2.text", []byte("12345"), 0644)
+	_ = afero.WriteFile(appFS, "/f/f3.xml", []byte("123456"), 0644)
+	_ = afero.WriteFile(appFS, "/f/f4.pdf", []byte("1234567"), 0644)
+	_ = afero.WriteFile(appFS, "/f/f5.html", []byte("12345678"), 0644)
+	_ = afero.WriteFile(appFS, "/f/f6.htm", []byte("12345678910"), 0644)
 
 	var tests = []struct {
 		cmdline []string
 	}{
 		{[]string{"a", "-p", "/"}},
+		{[]string{"a", "-p", "/", "-t", "3"}},
 		{[]string{"a", "-p", "/", "-r", "1"}},
 		{[]string{"a", "-p", "/f", "-r", "1", "-o"}},
 		{[]string{"a", "-p", "/", "-m"}},
