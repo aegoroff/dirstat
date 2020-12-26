d387e11f5a1821da04c5bca3c1995564e8794a5c
egoroff <egoroff@gmail.com>
2020-12-26 07:50:54 +0300

    refactoring: use IntHashSet to simplify file detail module implementation

--- module/file_detail.go
+++ module/file_detail.go
@@ -2,16 +2,17 @@ package module
 
 import (
 	"dirstat/module/internal/sys"
+	c9s "github.com/aegoroff/godatastruct/collections"
 	"sort"
 )
 
 type detailFileWorker struct {
 	voidFinalize
-	distribution     map[Range]files
-	enabledRanges    []int
-	enabledRangesMap map[int]bool
-	fileRanges       ranges
-	pd               decorator
+	voidInit
+	distribution map[Range]files
+	enabled      c9s.IntHashSet
+	fileRanges   ranges
+	pd           decorator
 }
 
 type detailFileRenderer struct {
@@ -20,11 +21,13 @@ type detailFileRenderer struct {
 }
 
 func newDetailFileWorker(rs ranges, enabledRanges []int, pd decorator) *detailFileWorker {
+	er := make(c9s.IntHashSet)
+	er.AddRange(enabledRanges...)
 	w := detailFileWorker{
-		enabledRanges: enabledRanges,
-		distribution:  make(map[Range]files, len(rs)),
-		fileRanges:    rs,
-		pd:            pd,
+		enabled:      er,
+		distribution: make(map[Range]files, len(rs)),
+		fileRanges:   rs,
+		pd:           pd,
 	}
 
 	return &w
@@ -39,19 +42,12 @@ func newDetailFileRenderer(work *detailFileWorker, order int) renderer {
 
 // Worker methods
 
-func (m *detailFileWorker) init() {
-	m.enabledRangesMap = make(map[int]bool)
-	for _, x := range m.enabledRanges {
-		m.enabledRangesMap[x] = true
-	}
-}
-
 func (m *detailFileWorker) handler(evt *sys.ScanEvent) {
 	f := evt.File
 	// Calculate files range statistic
 	for i, r := range m.fileRanges {
 		// Store each file info within range only if this this file size detail option set
-		if !r.Contains(f.Size) || !m.enabledRangesMap[i+1] {
+		if !r.Contains(f.Size) || !m.enabled.Contains(i+1) {
 			continue
 		}
 
@@ -67,23 +63,25 @@ func (m *detailFileWorker) handler(evt *sys.ScanEvent) {
 // Renderer method
 
 func (m *detailFileRenderer) print(p printer) {
-	if len(m.enabledRanges) > 0 {
-		heads := m.fileRanges.heads(numPrefixDecorator)
-		p.cprint("\n<gray>Detailed files stat:</>\n")
-		for i, r := range m.fileRanges {
-			if len(m.distribution[r]) == 0 {
-				continue
-			}
+	if m.enabled.Count() == 0 {
+		return
+	}
 
-			p.cprint("<gray>%s</>\n", heads[i])
+	heads := m.fileRanges.heads(numPrefixDecorator)
+	p.cprint("\n<gray>Detailed files stat:</>\n")
+	for i, r := range m.fileRanges {
+		if len(m.distribution[r]) == 0 {
+			continue
+		}
 
-			files := m.distribution[r]
-			sort.Sort(sort.Reverse(files))
+		p.cprint("<gray>%s</>\n", heads[i])
 
-			for _, f := range files {
-				size := human(f.size)
-				p.cprint("   %s - <yellow>%s</>\n", f, size)
-			}
+		files := m.distribution[r]
+		sort.Sort(sort.Reverse(files))
+
+		for _, f := range files {
+			size := human(f.size)
+			p.cprint("   %s - <yellow>%s</>\n", f, size)
 		}
 	}
 }
