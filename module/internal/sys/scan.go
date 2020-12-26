d110f9beb1c739d4e1bc0f07892a24e35c54880b
egoroff <egoroff@gmail.com>
2020-12-26 19:45:36 +0300

    refactoring: walker code moved to separate file

--- module/internal/sys/scan.go
+++ module/internal/sys/scan.go
@@ -2,9 +2,7 @@ package sys
 
 import (
 	"github.com/spf13/afero"
-	"os"
 	"path/filepath"
-	"sync"
 )
 
 // Handler defines scanning handler interface that handles filesystem events
@@ -38,14 +36,6 @@ type FolderEntry struct {
 
 	// The number of files in a folder
 	Count int64
-}
-
-type walker struct {
-	fs         afero.Fs
-	wg         sync.WaitGroup
-	mu         sync.RWMutex
-	queue      []string
-	restrictor chan struct{}
 }
 
 type filesystemItem struct {
@@ -142,107 +132,4 @@ func walkBreadthFirst(path string, fs afero.Fs, results chan<- *filesystemItem) 
 			ql = bf.len()
 		}
 	}
-}
-
-func newWalker(fs afero.Fs, parallel int) *walker {
-	bf := &walker{
-		fs:         fs,
-		queue:      make([]string, 0),
-		restrictor: make(chan struct{}, parallel),
-	}
-	return bf
-}
-
-func (bf *walker) peek() string {
-	bf.mu.RLock()
-	defer bf.mu.RUnlock()
-	return bf.queue[0]
-}
-
-func (bf *walker) pop() int {
-	bf.mu.Lock()
-	bf.mu.Unlock()
-	bf.queue = bf.queue[1:]
-	return len(bf.queue)
-}
-
-func (bf *walker) push(s string) {
-	bf.mu.Lock()
-	bf.queue = append(bf.queue, s)
-	bf.mu.Unlock()
-}
-
-func (bf *walker) len() int {
-	bf.mu.RLock()
-	defer bf.mu.RUnlock()
-	return len(bf.queue)
-}
-
-func (bf *walker) wait() {
-	bf.wg.Wait()
-}
-
-func (bf *walker) walk(d string, results chan<- *filesystemItem) {
-	defer bf.wg.Done()
-
-	entries := bf.readdir(d)
-
-	// Folder stat
-	var count int64
-	var size int64
-
-	for _, entry := range entries {
-		// Queue subdirs to walk in a queue
-		if entry.isDir {
-			bf.push(filepath.Join(d, entry.name))
-		} else {
-			// Send to channel
-			fileEvent := filesystemItem{
-				dir:   d,
-				name:  entry.name,
-				event: fsEventFile,
-				count: 1,
-				size:  entry.size,
-			}
-			results <- &fileEvent
-
-			// update folder stat
-			count++
-			size += entry.size
-		}
-	}
-
-	dirEvent := filesystemItem{
-		dir:   d,
-		event: fsEventDir,
-		count: count,
-		size:  size,
-	}
-	results <- &dirEvent
-}
-
-func (bf *walker) readdir(path string) []*filesysEntry {
-	bf.restrictor <- struct{}{}
-	defer func() { <-bf.restrictor }()
-	f, err := bf.fs.Open(path)
-	if err != nil {
-		return []*filesysEntry{}
-	}
-	defer Close(f)
-
-	entries, err := f.Readdir(-1)
-	if err != nil {
-		return []*filesysEntry{}
-	}
-
-	result := make([]*filesysEntry, 0, len(entries))
-	for _, e := range entries {
-		// dont follow symlinks
-		if e.Mode()&os.ModeSymlink == 0 {
-			fi := filesysEntry{name: e.Name(), size: e.Size(), isDir: e.IsDir()}
-			result = append(result, &fi)
-		}
-	}
-
-	return result
 }
