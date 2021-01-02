f508f500bf6cf7d9c8e73d9f2da7d5ba50791af0
egoroff <egoroff@gmail.com>
2021-01-02 19:35:37 +0300

    feature: colorizing percents if they are greater then some values

--- internal/module/transformers.go
+++ internal/module/transformers.go
@@ -3,10 +3,18 @@ package module
 import (
 	"fmt"
 	"github.com/dustin/go-humanize"
+	"github.com/gookit/color"
 )
 
 func percentTransformer(val interface{}) string {
-	return fmt.Sprintf("%.2f%%", val)
+	v := val.(float64)
+	if v >= 90.0 {
+		return color.Sprintf("<red>%.2f%%</>", v)
+	}
+	if v >= 70.0 {
+		return color.Sprintf("<yellow>%.2f%%</>", v)
+	}
+	return fmt.Sprintf("%.2f%%", v)
 }
 
 func sizeTransformer(val interface{}) string {
