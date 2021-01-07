af6c9bc95deb9162c6d21dbf9553bd84023ba452
egoroff <egoroff@gmail.com>
2021-01-07 09:13:01 +0300

    docs: comment to Open added

--- scan/filesystem.go
+++ scan/filesystem.go
@@ -9,6 +9,10 @@ func NewOsFs() Filesystem {
 	return &osFs{}
 }
 
+// Open opens the named file for reading.
+// If successful, methods on the returned file can be used for reading;
+// the associated file descriptor has mode os.O_RDONLY.
+// If there is an error, it will be of type *os.PathError.
 func (*osFs) Open(name string) (File, error) {
 	return os.Open(name)
 }
